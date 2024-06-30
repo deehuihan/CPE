2309c9c71c378e1071ec5194fa4abdb003432edd
deehuihan <72551081+deehuihan@users.noreply.github.com>
2024-06-30 22:58:19 +0800

    Update 10182-BeeMaja.cpp

--- 10182-BeeMaja.cpp
+++ 10182-BeeMaja.cpp
@@ -1,57 +1,66 @@
 #include <iostream>
-int varx, n , x, y;
+int v, n , x, y;
 
+//v 是層
+//n 是 input
+//x y 是maja 位置
 using namespace std;
 
 void solve()
 {
     while(true)
     {
-        if(n == 1)
+        if(n == 1) // 如果n是1的話表示在 (0,0) 這個位置，所以return 就好了，拿到 0,0
             return;
 
-        varv++;
-        y++;
-        n--;
+        v++; // 到下一層
+        y++; // 移動起始位置
+        n--; // 表示處理了一個蜂巢
 
         if(n == 1)
             return;
-
-        for (int i = 0; i < varv -1; i++) {
+        //這個是因為我們換到下一層，我們需要看一下每一個方向的蜂巢
+        for (int i = 0; i < v -1; i++) {  //第一個方向
             x--;
             y++;
-            if(--n == 1)
+            n--;
+            if(n == 1)
                 return;
         }
 
-        for (int i = 0; i < varv; i++) {
+        for (int i = 0; i < v; i++) {
             x--;
-            if(--n == 1)
+            n--;
+            if(n == 1)
                 return;
         }
 
-        for (int i = 0; i < varv; i++) {
+        for (int i = 0; i < v; i++) {
             y--;
-            if(--n == 1)
+            n--;
+            if(n == 1)
                 return;
         }
 
-        for (int i = 0; i < varv; i++) {
+        for (int i = 0; i < v; i++) {
             x++;
             y--;
-            if(--n == 1)
+            n--;
+            if(n == 1)
                 return;
         }
 
-        for (int i = 0; i < varv; i++) {
+        for (int i = 0; i < v; i++) {
             x++;
-            if(--n == 1)
+            n--;
+            if(n == 1)
                 return;
         }
 
-        for (int i = 0; i < varv; i++) {
+        for (int i = 0; i < v; i++) {
             y++;
-            if( --n== 1)
+            n--;
+            if(n== 1)
                 return;
         }
     }
@@ -61,7 +70,7 @@ int main()
 {
     while(cin >> n)
     {
-        varv = 0;
+        v = 0;
         x = 0;
         y = 0;
 
